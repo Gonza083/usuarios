7163b79e4c72f306679e558a0800654e24368963
Gonzalo Diaz <kasparov083@gmail.com>
2022-10-25 19:10:13 -0300

    agregamos logaut

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home,name="home"),
     path('signup/', views.signup, name="signup"),
     path('signin/', views.signin, name="signin"),
-    path('logout/',views.logout, name="logout")
+    path('logout/',views.singout, name="logout")
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 
 
 def home(request):
     return render(request, "home.html", {})
 
 def signup(request):
-    return HttpResponse("estas en el signup")
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('task')
+            except:
+                return render(request, 'signup.html', {
+                    'error': 'Username already exists ',
+                    'form': UserCreationForm,
+                    })
+        return render(request, 'signup.html', {
+            'error': 'password do not match',
+            'form': UserCreationForm,
+            })
 
 def signin(request):
     return HttpResponse("estas en el signin")
 
-def logout(request):
-    return HttpResponse("etas en el logout")
+def singout(request):
+    logout(request)
+    return redirect('home')
+
